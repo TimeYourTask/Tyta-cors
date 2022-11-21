eaf8aedfe1c3b26a0f8f3dafc1ebf361b8be689c
Quentin Aubert <9610898-quentin.aubert@users.noreply.gitlab.com>
2022-11-21 22:22:00 +0100

    chore(docker): dockerize api environment

--- src/server.js
+++ src/server.js
@@ -4,25 +4,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-require('dotenv').config();
 
-const port = process.env.SERVER_PORT || 3000;
-const db = process.env.DB_URL || 'mongodb://localhost:27017/tyta';
+const PORT = process.env.PORT;
+const DB = process.env.DB_URL;
 
 // Implement mongoose
 mongoose
-  .connect(db)
+  .connect(DB)
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.log('Could not connect to MongoDB', err));
 
-// Create a basic route
+//Create a basic route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.use(express.json());
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+//Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
