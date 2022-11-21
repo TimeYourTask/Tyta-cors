dd64b52ea2b87fd95daab186ed55a47a4e1467f6
Kilian Pichard <kil.pichard@icloud.com>
2022-11-21 14:56:25 +0100

    add dotenv for .env files

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 //Create a basic express server
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.SERVER_PORT || 3000;
 
 //Create a basic route
