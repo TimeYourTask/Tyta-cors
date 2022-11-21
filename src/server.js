8b0521b1f523fe11977cbc46dfc4957575379827
Kilian Pichard <kil.pichard@icloud.com>
2022-11-21 14:20:12 +0100

    feat(TYTA-21/TYTA-22): Implement basic nodejs server with express

---
+++ src/server.js
@@ -0,0 +1,14 @@
+//Create a basic express server
+const express = require("express");
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+
+//Create a basic route
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+//Start the server
+app.listen(port, () => {
+	console.log(`Server listening at http://localhost:${port}`);
+});
