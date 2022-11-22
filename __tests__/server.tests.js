f5ddc74a4c711c1bb59a117a1e2668a93befa76d
Kilian Pichard <7956666-kilianp@users.noreply.gitlab.com>
2022-11-22 16:37:14 +0100

    feat(TYTA-27): add testing library in local

---
+++ __tests__/server.tests.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../src/app');
+
+require('dotenv').config();
+
+/* Connecting to the database before each test. */
+beforeEach(async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+});
+
+/* Closing database connection after each test. */
+afterEach(async () => {
+  await mongoose.connection.close();
+});
+
+/* Testing the GET / route. */
+describe('GET /', () => {
+  it('should return 200 OK', async () => {
+    const res = await request(app).get('/');
+    expect(res.status).toBe(200);
+  });
+});
--- src/server.js
+++ src/server.js
@@ -1,27 +1,18 @@
+require('dotenv').config();
+
 // Create a basic express server
-
-const express = require('express');
 const mongoose = require('mongoose');
-
-const app = express();
-
-const PORT = process.env.PORT;
-const DB = process.env.DB_URL;
+const app = require('./app');
+const PORT = process.env.SERVER_PORT || 3000;
 
 // Implement mongoose
 mongoose
-  .connect(DB)
-  .then(() => console.log('Connected to MongoDB successfully'))
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    app.listen(PORT, () => {
+      console.log(`Server listening at http://localhost:${PORT}`);
+    });
+  })
   .catch((err) => console.log('Could not connect to MongoDB', err));
-
-//Create a basic route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.use(express.json());
-
-//Start the server
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
