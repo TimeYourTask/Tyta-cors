37d87e120a6b2c9e81548f85995dfa8822fb914c
Rémi Rubis <remi.rubis@yahoo.com>
2022-12-01 23:14:28 +0100

    feat(controller/task): create task controller

--- src/controllers/taskCtrl.js
+++ src/controllers/taskCtrl.js
@@ -15,6 +15,7 @@ exports.getTasks = (req, res) => {
 };
 
 exports.getOneTask = (req, res) => {
+<<<<<<< HEAD
   Task.findById(req.params.task_id)
     .then((task) => {
       if (!task) {
@@ -22,10 +23,15 @@ exports.getOneTask = (req, res) => {
       }
       return res.status(200).json({ task });
     })
+=======
+  Task.findById(req.params.TaskId)
+    .then((task) => res.status(200).json({ task }))
+>>>>>>> 12c9d6c (feat(controller/task): create task controller)
     .catch((error) => res.status(400).json({ error }));
 };
 
 exports.deleteTask = (req, res) => {
+<<<<<<< HEAD
   Task.findByIdAndDelete(req.params.task_id)
     .then((task) => {
       if (!task) {
@@ -35,12 +41,17 @@ exports.deleteTask = (req, res) => {
       }
       return res.status(200).json({ message: 'Task deleted!' });
     })
+=======
+  Task.findByIdAndDelete(req.params.TaskId)
+    .then(() => res.status(200).json({ message: 'Task deleted!' }))
+>>>>>>> 12c9d6c (feat(controller/task): create task controller)
     .catch((error) =>
       res.status(400).json({ message: 'Invalid Request!', error })
     );
 };
 
 exports.updateTask = (req, res) => {
+<<<<<<< HEAD
   console.log(req.params);
   Task.findByIdAndUpdate(req.params.task_id, req.body, {
     new: true,
@@ -52,6 +63,14 @@ exports.updateTask = (req, res) => {
           .json({ message: 'No items updated : task not found!' });
       }
       return res
+=======
+  Task.findByIdAndUpdate(req.params.TaskId, req.body, {
+    new: true,
+    upsert: true,
+  })
+    .then((task) => {
+      res
+>>>>>>> 12c9d6c (feat(controller/task): create task controller)
         .status(200)
         .json({ message: 'The Task has been modified correclty!', task });
     })
