e76b847ca57cb957e896ea193199d039afe7cf57
Rémi Rubis <remi.rubis@yahoo.com>
2022-12-03 18:24:59 +0100

    feat(reset-password): add reset password routes in server file

--- src/server.js
+++ src/server.js
@@ -8,10 +8,10 @@ const userRoute = require('./routes/userRoute');
 const teamRoute = require('./routes/teamRoute');
 const projectRoute = require('./routes/projectRoute');
 const taskRoute = require('./routes/taskRoute');
+const resetPasswordRoute = require('./routes/resetPasswordRoute');
 
 const app = express();
 const PORT = process.env.SERVER_PORT;
-const DB = process.env.DB_URL;
 
 // Implement mongoose
 mongoose
@@ -21,6 +21,7 @@ mongoose
     app.use(express.json());
 
     userRoute(app);
+    resetPasswordRoute(app);
     teamRoute(app);
     projectRoute(app);
     taskRoute(app);
