45182b44c15911ae687c5242c148843b23651608
Rémi Rubis <remi.rubis@yahoo.com>
2022-12-03 18:24:29 +0100

    refactor(user.route): edit user routes for login and register

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,11 @@
 const userCtrl = require('../controllers/userCtrl');
-const {authMiddleware} = require("../middleware/authMiddleware");
-
+const { authMiddleware } = require('../middleware/authMiddleware');
 
 module.exports = (app) => {
-  app.get('/users', authMiddleware, userCtrl.getUsers);
-  app.get('/user/:userId', authMiddleware, userCtrl.getOneUser);
-  app.delete('/user/:userId', authMiddleware, userCtrl.deleteUser);
-  app.put('/user/:userId', authMiddleware, userCtrl.updateUser);
+  app.post('/register', userCtrl.userRegister);
+  app.post('/login', userCtrl.userLogin);
+  app.get('/users', userCtrl.getUsers);
+  app.get('/user/:userId', userCtrl.getOneUser);
+  app.delete('/user/:userId', userCtrl.deleteUser);
+  app.put('/user/:userId', userCtrl.updateUser);
 };
