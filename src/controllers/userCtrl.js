421140c9c753c8fdd4b830f4516e6998dca394f9
Rémi Rubis <remi.rubis@yahoo.com>
2022-12-04 17:18:42 +0100

    refactor(controller/user): change logic of reset password to use token id instead of user id

--- src/controllers/userCtrl.js
+++ src/controllers/userCtrl.js
@@ -121,7 +121,7 @@ exports.resetPassword = async (req, res) => {
     if (!user) {
       return res.status(200).json({
         message:
-          'We have sent an email to this address, if it exists in our data we invite you to look in a few moments.',
+          'If our services find a match with the address you entered, you will receive a reset link in a few moments.',
       });
     }
 
@@ -133,11 +133,11 @@ exports.resetPassword = async (req, res) => {
       }).save();
     }
 
-    const link = `${process.env.FRONTEND_URL}/reset-password?token=${token.token}&id=${user.id}`;
+    const link = `${process.env.FRONTEND_URL}/reset-password?token=${token.token}&id=${token.id}`;
     await sendEmail(user.email, 'Password Reset', link);
     return res.status(200).json({
       message:
-        'We have sent an email to this address, if it exists in our data we invite you to look in a few moments.',
+        'If our services find a match with the address you entered, you will receive a reset link in a few moments.',
     });
   } catch (error) {
     return res.status(400).json({ message: 'Something went wrong!' });
