31c24e3976fedc32ab5846a9d577157ed119a32a
Rémi Rubis <remi.rubis@yahoo.com>
2023-01-04 23:31:10 +0100

    refactor(routes): add comments and add authorization middleware on specific routes

--- src/routes/teamRoute.js
+++ src/routes/teamRoute.js
@@ -1,24 +1,47 @@
 const teamCtrl = require('../controllers/teamCtrl');
-const { authMiddleware } = require('../middleware/authMiddleware');
+
+const { authMiddleware } = require('../middleware/auth.middleware');
+const { isAuthorized } = require('../middleware/authorization.middleware');
+const { ROLES } = require('../config/global');
 
 module.exports = (app) => {
+  // Create a team
   app.post('/team', authMiddleware, teamCtrl.createTeam);
-  app.get('/teams', authMiddleware, teamCtrl.getTeams);
+
+  // Get all user teams
   app.get('/teams/mine', authMiddleware, teamCtrl.getMyTeams);
+
+  // Get a team by id
   app.get('/team/:teamId', authMiddleware, teamCtrl.getOneTeam);
+
+  // Get all users in a team with his id
   app.get('/team/:teamId/users', authMiddleware, teamCtrl.getUserOfTeam);
-  app.delete('/team/:teamId', authMiddleware, teamCtrl.deleteTeam);
+
+  // Edit a team
   app.put('/team/:teamId', authMiddleware, teamCtrl.updateTeamName);
+
+  // Add a user to a team
   app.put('/team/:teamId/user/', authMiddleware, teamCtrl.addUserToTeam);
+
+  // Delete a user from a team
   app.delete(
     '/team/:teamId/user/',
     authMiddleware,
     teamCtrl.removeUserFromTeam
   );
-  app.put('/team/:teamId/project/', authMiddleware, teamCtrl.addProjectToTeam);
-  app.delete(
-    '/team/:teamId/project/',
+
+  // Remove a team
+  app.delete('/team/:teamId', authMiddleware, teamCtrl.deleteTeam);
+
+  /**
+   * Admin routes
+   */
+
+  // Get all teams
+  app.get(
+    '/admin/teams',
     authMiddleware,
-    teamCtrl.removeProjectFromTeam
+    isAuthorized(ROLES.admin),
+    teamCtrl.getTeams
   );
 };
--- src/routes/timeTaskRoute.js
+++ src/routes/timeTaskRoute.js
@@ -1,38 +1,63 @@
 const timeTaskCtrl = require('../controllers/timeTaskCtrl');
-const { authMiddleware } = require('../middleware/authMiddleware');
+
+const { authMiddleware } = require('../middleware/auth.middleware');
 const { verifyTaskIdParams } = require('../middleware/verifyTaskIdParams');
+const { isAuthorized } = require('../middleware/authorization.middleware');
+const { ROLES } = require('../config/global');
 
 module.exports = (app) => {
-  app.get('/timers', authMiddleware, timeTaskCtrl.getTimeTasks);
-  app.get('/task/user/timer', authMiddleware, timeTaskCtrl.getUserTimeTasks);
+  // Start a task
   app.post(
     '/task/:task_id/start',
     authMiddleware,
     verifyTaskIdParams,
     timeTaskCtrl.startTimeTask
   );
+
+  // Stop a task
   app.post(
     '/task/:task_id/end',
     authMiddleware,
     verifyTaskIdParams,
     timeTaskCtrl.endTimeTask
   );
+
+  // Get all timers of user current task
+  app.get('/task/user/timer', authMiddleware, timeTaskCtrl.getUserTimeTasks);
+
+  // Get all timers of a task
   app.get(
     '/task/:task_id/timer',
     authMiddleware,
     verifyTaskIdParams,
     timeTaskCtrl.getOneTimeTask
   );
+
+  // Get single timer by id in all list of task times
   app.put(
     '/task/:task_id/timer/:time_id',
     authMiddleware,
     verifyTaskIdParams,
     timeTaskCtrl.updateTimeTask
   );
+
+  // Remove timer of a task
   app.delete(
     '/task/:task_id/timer',
     authMiddleware,
     verifyTaskIdParams,
     timeTaskCtrl.deleteTimeTask
+  );
+
+  /**
+   * Admin routes
+   */
+
+  // Get all timers
+  app.get(
+    '/admin/timers',
+    authMiddleware,
+    isAuthorized(ROLES.admin),
+    timeTaskCtrl.getTimeTasks
   );
 };
