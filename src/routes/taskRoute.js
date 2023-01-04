c113b4f338084641c838c4d57676a14396c9a143
Rémi Rubis <remi.rubis@yahoo.com>
2023-01-04 14:23:52 +0100

    fix(project/tasks): solve removed route to fetch all tasks of a project

--- src/routes/taskRoute.js
+++ src/routes/taskRoute.js
@@ -5,6 +5,11 @@ module.exports = (app) => {
   app.post('/task', authMiddleware, taskCtrl.createTask);
   app.get('/tasks', authMiddleware, taskCtrl.getTasks);
   app.get('/task/:task_id', authMiddleware, taskCtrl.getOneTask);
+  app.get(
+    '/project/:projectId/tasks',
+    authMiddleware,
+    taskCtrl.getTasksByProject
+  );
   app.put('/task/:task_id', authMiddleware, taskCtrl.updateTask);
   app.delete('/task/:task_id', authMiddleware, taskCtrl.deleteTask);
   app.get('/tasks/user/', authMiddleware, taskCtrl.getUserTasks);
